f8558ccdd34a1aea8c8ce62298059d7aabdc0a43
Mo7amedShaban1 <mo7amed.shaban20@gmail.com>
2019-12-28 13:54:16 +0200

    Updates

---
+++ precache-manifest.59a5303a6d5ee18b9de256cc0fed0e43.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "dda6d65e3ba8e38ee3cb",
+    "url": "/static/css/main.9d4ac3ba.chunk.css"
+  },
+  {
+    "revision": "dda6d65e3ba8e38ee3cb",
+    "url": "/static/js/main.dda6d65e.chunk.js"
+  },
+  {
+    "revision": "57c0b4ec061491d5c971",
+    "url": "/static/js/1.57c0b4ec.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "e4fa9b2f1b6f0ddd57cb84851de43a50",
+    "url": "/index.html"
+  }
+];
