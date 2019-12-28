3ecda35b842900a71fd4b02980afc5f0d89f0a08
Mo7amedShaban1 <mo7amed.shaban20@gmail.com>
2019-12-28 14:00:41 +0200

    adding files for github pages

---
+++ build/precache-manifest.b92d63b39033203f4b253b27242be494.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3123c3cf2b37934d53c",
+    "url": "/my-reads/static/css/main.ffe3afc5.chunk.css"
+  },
+  {
+    "revision": "f3123c3cf2b37934d53c",
+    "url": "/my-reads/static/js/main.f3123c3c.chunk.js"
+  },
+  {
+    "revision": "57c0b4ec061491d5c971",
+    "url": "/my-reads/static/js/1.57c0b4ec.chunk.js"
+  },
+  {
+    "revision": "d3c91a85274073bd8c52",
+    "url": "/my-reads/static/js/runtime~main.d3c91a85.js"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/my-reads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/my-reads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/my-reads/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "447e6241f9709f2ed1dd24276a53dffe",
+    "url": "/my-reads/index.html"
+  }
+];
